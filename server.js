c8dde5e6ada16c6ec5febf0d9b7cdbc29e07e69b
shahzaib <kshahzaib07@gmail.com>
2026-09-19 19:57:26 -0700

    error-handling middleware

--- server.js
+++ server.js
@@ -12,6 +12,15 @@ app.use("/products" , productRoutes)
 app.use("/users" , userRoutes)
 
 
+app.use((err, req, res, next) => {
+  console.error(err);
+
+  res.status(500).json({
+    status: false,
+    message: "Internal Server Error"
+  });
+});
+
 app.listen(3000 , () => {
   console.log("Server running on port 3000")
 })
